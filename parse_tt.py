17d877e0deaaf28014229b3dc04593c9a42771e9
Bechir Zalila <bechir.zalila@gmail.com>
2018-12-29 09:43:47 +0000

    * Fin parsing
    git-svn-id: https://svn.zalila.net/svn/ulysse/work/devs/timetables@14586 643dc046-8b15-0410-bf49-ad7b499e6f5d

--- parse_tt.py
+++ parse_tt.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+# $Id$
 
 from xml.etree import ElementTree as et
 
@@ -25,9 +26,22 @@ def getPropertyText (elt, prop):
 
 def getName (elt):
     return getPropertyText (elt, 'Name')
+
+def active (elt):
+    return getPropertyText (elt, 'Active') == 'true'
+
+def permanentlyLocked (elt):
+    return getPropertyText (elt, 'Permanently_Locked') == 'true'
+
+def emptyDay ():
+    return {h : set() for h in r_dict ['hours']}
+
+def emptyWeek ():
+    return {d : emptyDay() for d in r_dict ['days']}
     
 def P_Unknown (elt):
-    print ("Unknown element: <" + elt.tag + ">")
+    pass
+    #print ("Unknown element: <" + elt.tag + ">")
 
 def Tree_Parse (elt, tree_dict):
     for e in elt.getchildren():
@@ -37,104 +51,93 @@ def Tree_Parse (elt, tree_dict):
             P_Unknown (e)
     
 def P_Institution_Name (elt):
-    print ("Parsing Institution Name")
+    print ("Parsing Institution Name: <" + elt.text + ">")
 
 def P_Comments (elt):
-    print ("Parsing Comments")
-
-def P_Number_of_Days (elt):
-    print ("Parsing Number of days")
+    print ("Parsing Comments: <" + elt.text + ">")
 
 def P_Day (elt):
     r_dict ['days'].add(getName (elt))
-    print ("Parsing Day")
     
 def P_Days_List (elt):
     r_dict['days'] = set()
-    Tree_Parse (elt, {'Number_of_Days' : P_Number_of_Days, 'Day' : P_Day})
-    print ("Parsing day list")
-
-def P_Number_of_Hours (elt):
-    print ("Parsing Number of hours")
+    Tree_Parse (elt, {'Day' : P_Day})
 
 def P_Hour (elt):
     r_dict ['hours'].add(getName (elt))
-    print ("Parsing Hour")
     
 def P_Hours_List (elt):
     r_dict['hours'] = set()
-    Tree_Parse (elt, {'Number_of_Hours' : P_Number_of_Hours, 'Hour' : P_Hour})
-    print ("Parsing hour list")
+    Tree_Parse (elt, {'Hour' : P_Hour})
 
 def P_Subject (elt):
     r_dict ['subjects'].add(getName (elt))
-    print ("Parsing Subject")
 
 def P_Subjects_List (elt):
     r_dict['subjects'] = set ()
     Tree_Parse (elt, {'Subject' : P_Subject})
-    print ("Parsing Subject list")
 
 def P_Activity_Tag (elt):
     r_dict ['activity_tags'].add(getName (elt))
-    print ("Parsing Activity Tag")
     
 def P_Activity_Tags_List (elt):
     r_dict['activity_tags'] = set ()
     Tree_Parse (elt, {'Activity_Tag' : P_Activity_Tag})
-    print ("Parsing Activity Tags list")
 
 def P_Teacher (elt):
-    t_dict = {'activities' : []}
+    t_dict = {'activities' : [], 'timetable' : emptyWeek()}
     r_dict ['teachers'][getName (elt)] = t_dict
-    print ("Parsing Teachers")
     
 def P_Teachers_List (elt):
     r_dict['teachers'] = {}
     Tree_Parse (elt, {'Teacher' : P_Teacher})
-    print ("Parsing Teachers list")
 
 stud_stack = []
 
 def P_Subgroup (elt):
     g_name = stud_stack[-1]
-    r_dict ['subgroups'][getName(elt)] = {'group' : g_name, 'activities' : []}
-    print ("Parsing Subgroup")
+    r_dict ['groups'][g_name]['subgroups'].add (getName(elt))
+    r_dict ['subgroups'][getName(elt)] = {
+        'group'      : g_name,
+        'activities' : [],
+        'timetable'  : emptyWeek()
+    }
 
 def P_Group (elt):
     y_name = stud_stack [-1]
-    r_dict ['groups'][getName (elt)] = {'year' : y_name, 'activities' : []}
+    r_dict ['years'][y_name]['groups'].add (getName(elt))
+    r_dict ['groups'][getName (elt)] = {
+        'year'       : y_name,
+        'activities' : [],
+        'subgroups'  : set()
+    }
     stud_stack.append (getName (elt))
     Tree_Parse (elt, {'Subgroup' : P_Subgroup})
     stud_stack.pop ()
-    print ("Parsing Group")
     
 def P_Year (elt):
-    r_dict['years'][getName (elt)] = {'activities' : []}
+    r_dict['years'][getName (elt)] = {
+        'activities' : [],
+        'groups'     : set()
+    }
     stud_stack.append (getName (elt))
     Tree_Parse (elt, {'Group' : P_Group})
     stud_stack.pop()
-    print ("Parsing Year")
     
 def P_Students_List (elt):
     r_dict['years']     = {}
     r_dict['groups']    = {}
     r_dict['subgroups'] = {}
     Tree_Parse (elt, {'Year' : P_Year})
-    print ("Parsing Students list")
 
 def InsertActivity (id, x):
     if x in r_dict ['teachers']:
-        # This is a teacher
         r_dict ['teachers'][x]['activities'].append (id)
     elif x in r_dict ['years']:
-        # This is a year
         r_dict ['years'][x]['activities'].append (id)
     elif x in r_dict ['groups']:
-        # This is a group
         r_dict ['groups'][x]['activities'].append (id)
     elif x in r_dict ['subgroups']:
-        # This is a subgroup
         r_dict ['subgroups'][x]['activities'].append (id)
     else:
         raise Exception ('InsertActivity', x)
@@ -145,67 +148,105 @@ def P_Activity (elt):
         'teachers' : [x.text for x in getPropertyList (elt, 'Teacher')],
         'tags'     : [x.text for x in getPropertyList (elt, 'ActivityTags')],
         'students' : [x.text for x in getPropertyList (elt, 'Students')],
-        'duration' : getProperty (elt, 'Duration').text
+        'duration' : int (getProperty (elt, 'Duration').text),
+        'day'      : None,
+        'hour'     : None,
+        'room'     : None,
     }
-
     id = getPropertyText (elt, 'Id')
     r_dict['activities'][id] = a_dict
-    
     for t in a_dict ['teachers']:
         InsertActivity (id, t)
-
     for s in a_dict ['students']:
         InsertActivity (id, s)
-
-    print ("Parsing Activity")
     
 def P_Activities_List (elt):
     r_dict['activities'] = {}
     Tree_Parse (elt, {'Activity' : P_Activity})
-    print ("Parsing Activities list")
 
 def P_Building (elt):
-    r_dict ['buildings'][getName (elt)] = set()
-    print ("Parsing Building")
+    r_dict ['buildings'][getName (elt)] = {'rooms' : set()}
     
 def P_Buildings_List (elt):
     r_dict['buildings'] = {}
     Tree_Parse (elt, {'Building' : P_Building})
-    print ("Parsing Buildings list")
 
 def P_Room (elt):
-    r_dict['buildings'][getPropertyText (elt, 'Building')].add (getName (elt))
-    print ("Parsing Room")
+    r_dict['rooms'][getName (elt)] = {
+        'timetable' : emptyWeek(),
+        'building'  : getPropertyText (elt, 'Building')
+    }
+    r_dict['buildings'][getPropertyText (elt, 'Building')]['rooms'].add (getName (elt))
     
 def P_Rooms_List (elt):
+    r_dict ['rooms'] = {}
     Tree_Parse (elt, {'Room' : P_Room})
-    print ("Parsing Rooms list")
 
+def P_ConstraintActivityPreferredStartingTime (elt):
+    if active (elt):
+        act_id = getPropertyText (elt, 'Activity_Id')
+        act_day = getPropertyText (elt, 'Preferred_Day')
+        act_hour = getPropertyText (elt, 'Preferred_Hour')
+
+        r_dict ['activities'][act_id]['day'] = act_day
+        r_dict ['activities'][act_id]['hour'] = act_hour
+
+        for t in r_dict ['activities'][act_id]['teachers']:
+            r_dict['teachers'][t]['timetable'][act_day][act_hour].add(act_id)
+
+        for s in r_dict ['activities'][act_id]['students']:
+            if s in r_dict ['subgroups']:
+                r_dict['subgroups'][s]['timetable'][act_day][act_hour].add(act_id)
+            elif s in r_dict ['groups']:
+                for sg in r_dict ['groups'][s]['subgroups']:
+                    r_dict['subgroups'][sg]['timetable'][act_day][act_hour].add(act_id)
+            elif s in r_dict ['years']:
+                for g in r_dict['years'][s]['groups']:
+                    for sg in r_dict ['groups'][g]['subgroups']:
+                        r_dict['subgroups'][sg]['timetable'][act_day][act_hour].add(act_id)
+            else:
+                raise Exception ('ConstraintActivityPreferredStartingTime', s)
+    
 def P_Time_Constraints_List (elt):
-    print ("Parsing Time Constraints list")
+    Tree_Parse (elt, {'ConstraintActivityPreferredStartingTime' :
+                      P_ConstraintActivityPreferredStartingTime})
 
+def P_ConstraintActivityPreferredRoom (elt):
+    # Take in account only constraints generated by Fet
+    if active (elt):
+        act_id = getPropertyText (elt, 'Activity_Id')
+        act_room = getPropertyText (elt, 'Room')
+        act_day = r_dict ['activities'][act_id]['day']
+        act_hour = r_dict ['activities'][act_id]['hour']
+
+        r_dict ['activities'][act_id]['room'] = act_room
+        r_dict ['rooms'][act_room]['timetable'][act_day][act_hour].add(act_id)
+    
 def P_Space_Constraints_List (elt):
-    print ("Parsing Space Constraints list")
+    Tree_Parse (elt, {'ConstraintActivityPreferredRoom' :
+                      P_ConstraintActivityPreferredRoom})
 
 xml = et.parse("2018-2019_Sem-2_ENIS_DGIMA_data_and_timetable.fet")
 
 root = xml.getroot()
 
 root_tree = {
-    'Institution_Name' : P_Institution_Name,
-    'Comments' : P_Comments,
-    'Days_List' : P_Days_List,
-    'Hours_List' : P_Hours_List,
-    'Subjects_List' : P_Subjects_List,
-    'Activity_Tags_List' : P_Activity_Tags_List,
-    'Teachers_List' : P_Teachers_List,
-    'Students_List' : P_Students_List,
-    'Activities_List' : P_Activities_List,
-    'Buildings_List' : P_Buildings_List,
-    'Rooms_List' : P_Rooms_List,
-    'Time_Constraints_List' : P_Time_Constraints_List,
+    'Institution_Name'       : P_Institution_Name,
+    'Comments'               : P_Comments,
+    'Days_List'              : P_Days_List,
+    'Hours_List'             : P_Hours_List,
+    'Subjects_List'          : P_Subjects_List,
+    'Activity_Tags_List'     : P_Activity_Tags_List,
+    'Teachers_List'          : P_Teachers_List,
+    'Students_List'          : P_Students_List,
+    'Activities_List'        : P_Activities_List,
+    'Buildings_List'         : P_Buildings_List,
+    'Rooms_List'             : P_Rooms_List,
+    'Time_Constraints_List'  : P_Time_Constraints_List,
     'Space_Constraints_List' : P_Space_Constraints_List
 }
 
 Tree_Parse (root, root_tree)
-print (r_dict)
+print (r_dict['rooms']['8103']['timetable']['Lundi'])
+print (r_dict['teachers']['Bechir ZALILA']['timetable']['Lundi'])
+print (r_dict['subgroups']['GI1-S1-G1']['timetable']['Lundi'])
